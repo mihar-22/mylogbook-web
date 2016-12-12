4af382175d9effa29b8d4b93431aea3d053b4539
mihar-22 <rahim_alwer@hotmail.com>
2016-12-12 19:01:29 +1100

    reset password notification

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', url('password/reset', [$notifiable->email, $this->token]))
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
