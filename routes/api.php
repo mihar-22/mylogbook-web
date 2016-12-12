a2c91c21e6107021437d121684561ecde2f9fb11
mihar-22 <rahim_alwer@hotmail.com>
2016-12-12 18:46:39 +1100

    API routes for users and forgot password

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::group(['prefix' => 'v1'], function () {
+	Route::group(['prefix' => 'users'], function () {
+		Route::post('register', 'AuthController@register');
+		
+		Route::post('login', 'AuthController@login');
+		
+		Route::post('logout', 'AuthController@logout');
+	});
+
+	Route::post('password/forgot', 'PasswordController@sendResetLink');
+});
