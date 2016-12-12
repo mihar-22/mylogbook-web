226ea62b65dbb7c3a9106890ae41062e994de6af
mihar-22 <rahim_alwer@hotmail.com>
2016-12-12 18:55:58 +1100

    verify email notification

---
+++ app/Notifications/VerifyEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class VerifyEmail extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting('One more step!')
+                    ->line('Please verify your email so we can be sure that it is yours.')
+                    ->action('Verify Email', url('email/verify', [$notifiable->email, $this->token]))
+                    ->line('Once you are verified you can jump right into the application!');
+    }
+}
