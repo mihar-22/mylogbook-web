be987b9271c2ae513a7fbd0d89c22c98e9527458
mihar-22 <rahim_alwer@hotmail.com>
2016-12-12 19:05:26 +1100

    create response for authentication exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Facades\ApiResponder;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
@@ -57,7 +58,8 @@ class Handler extends ExceptionHandler
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
+            return ApiResponder::respondWithMessage('unauthenticated')
+                                 ->setStatusCode(401);
         }
 
         return redirect()->guest('login');
