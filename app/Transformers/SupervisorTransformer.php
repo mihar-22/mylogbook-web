5fc1cbb28f63ce7841005e5b63d189f87ebe9ac7
mihar-22 <rahim_alwer@hotmail.com>
2016-12-14 18:04:05 +1100

    supervisor transformer

---
+++ app/Transformers/SupervisorTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Supervisor;
+use League\Fractal\TransformerAbstract;
+
+class SupervisorTransformer extends TransformerAbstract
+{
+    public function transform(Supervisor $supervisor)
+    {
+        return [
+            'id' => (int) $supervisor->id,
+            'first_name' => $supervisor->first_name,
+            'last_name' => $supervisor->last_name,
+            'gender' => $supervisor->gender,
+            'avatar' => (int) $supervisor->avatar
+        ];
+    }
+}
