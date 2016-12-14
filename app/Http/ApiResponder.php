7de6cc168ea3ce38a9850ba3a7bad0516c1decdb
mihar-22 <rahim_alwer@hotmail.com>
2016-12-14 14:22:05 +1100

    API responder - respond with collection

--- app/Http/ApiResponder.php
+++ app/Http/ApiResponder.php
@@ -3,9 +3,20 @@
 namespace App\Http;
 
 use Illuminate\Http\Response;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Serializer\DataArraySerializer;
+use League\Fractal\TransformerAbstract;
 
 class ApiResponder
 {
+    private $manager;
+
+    public function __construct(Manager $manager)
+    {
+        $this->manager = $manager;
+    }
+
     public function respondWithMessage($message)
     {
         return $this->respond($message);
@@ -21,14 +32,30 @@ class ApiResponder
         return $this->respond($message, null, $errors);
     }
 
+    public function respondWithCollection($collection, TransformerAbstract $transformer)
+    {
+        $message = "collection of {$collection[0]->getTable()}";
+
+        $resource = new Collection($collection, $transformer);
+
+        $data = $this->serialize($resource)['data']; 
+
+        return $this->respond($message, $data);
+    }
+
     private function respond($message = '', $data = null, $errors = null)
     {
         $response = array_filter([
-            'message' => $message,
-            'data' => $data,
+            'message' => $message, 
+            'data' => $data, 
             'errors' => $errors
         ]);
 
         return response($response);
+    }
+
+    private function serialize($resource)
+    {
+        return $this->manager->createData($resource)->toArray();
     }
 }
