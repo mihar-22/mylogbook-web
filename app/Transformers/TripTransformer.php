97b69673682d61d030f4de30da9f532a1e049656
mihar-22 <rahim_alwer@hotmail.com>
2016-12-15 16:26:08 +1100

    trip transformer

---
+++ app/Transformers/TripTransformer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Trip;
+use League\Fractal\TransformerAbstract;
+
+class TripTransformer extends TransformerAbstract
+{
+    public function transform(Trip $trip)
+    {
+        return [
+            'id' => (int) $trip->id,
+            'date' => $trip->date,
+            'seconds'  => (int) $trip->seconds,
+            'odometer' => (int) $trip->odometer,     
+            'distance' => (double) $trip->distance,
+            'car_id' => (int) $trip->car_id,
+            'supervisor_id' => (int) $trip->supervisor_id,
+            'light' => [
+                'day' => (boolean) $trip->day,
+                'afternoon' => (boolean) $trip->afternoon,
+                'night' => (boolean) $trip->night
+            ],
+            'weather' => [
+                'clear' => (boolean) $trip->clear,
+                'rain' => (boolean) $trip->rain,
+                'thunder' => (boolean) $trip->thunder
+            ],
+            'traffic' => [
+                'light' => (boolean) $trip->light,
+                'moderate' => (boolean) $trip->moderate,
+                'heavy' => (boolean) $trip->traffic
+            ],
+            'roads' => [
+                'local_street' => (boolean) $trip->local_street,
+                'main_road' => (boolean) $trip->main_road,
+                'inner_city' => (boolean) $trip->inner_city,
+                'freeway' => (boolean) $trip->freeway,
+                'rural_highway' => (boolean) $trip->rural_highway,
+                'gravel' => (boolean) $trip->gravel
+            ]
+        ];
+    }
+}
