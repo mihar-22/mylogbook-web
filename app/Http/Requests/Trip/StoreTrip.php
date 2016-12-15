9afbf00e0bf6820393bc301fab778ec58a7f748a
mihar-22 <rahim_alwer@hotmail.com>
2016-12-15 16:27:06 +1100

    trip form requests

---
+++ app/Http/Requests/Trip/StoreTrip.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests\Trip;
+
+use App\Http\Requests\FormatResponse;
+use App\Models\Car;
+use App\Models\Supervisor;
+use App\Models\Trip;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreTrip extends FormRequest
+{
+	use FormatResponse;
+	
+    public function authorize()
+    {   
+        $car = Car::find($this->request->get('car_id'));
+
+        $supervisor = Supervisor::find($this->request->get('supervisor_id'));
+
+        return $car &&
+               $supervisor &&
+               policy(Trip::class)->create(Auth::user(), $car, $supervisor);
+    }
+
+    public function rules()
+    {
+        return [
+            'date' => 'required|date|after:today',
+            'seconds' => 'required|integer|max:259200',
+            'odometer' => 'required|integer|max:999999',
+            'distance' => 'required|regex:/^(\d{2,6})(\.)(\d{2})$/',
+
+            // Light
+            'day' => 'boolean',
+            'afternoon' => 'boolean',
+            'night' => 'boolean',
+
+            // Weather
+            'clear' => 'boolean',
+            'rain' => 'boolean',
+            'thunder' => 'boolean',
+
+            // Traffic
+            'light' => 'boolean',
+            'moderate' => 'boolean',
+            'heavy' => 'boolean',
+
+            // Roads
+            'local_street' => 'boolean',
+            'main_road' => 'boolean',
+            'inner_city' => 'boolean',
+            'freeway' => 'boolean',
+            'rural_highway' => 'boolean',
+            'gravel' => 'boolean',
+
+            // Resources
+            'car_id' => 'required|integer',
+            'supervisor_id' => 'required|integer'
+        ];  
+    }
+}
