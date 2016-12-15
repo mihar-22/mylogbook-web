0ba40b2e5975beb8549d8b3f3e01f3344351ff43
mihar-22 <rahim_alwer@hotmail.com>
2016-12-15 16:26:49 +1100

    trip controller

---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Facades\ApiResponder;
+use App\Http\Requests\Trip\StoreTrip;
+use Illuminate\Support\Facades\Auth;
+
+class TripController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(StoreTrip $request)
+    {
+        $trip = $request->only(
+            'date',
+            'seconds',
+            'odometer',     
+            'distance',
+
+            // Light
+            'day',
+            'afternoon',
+            'night',
+
+            // Weather
+            'clear',
+            'rain',
+            'thunder',
+
+            // Traffic
+            'light',
+            'moderate',
+            'heavy',
+
+            // Roads
+            'local_street',
+            'main_road',
+            'inner_city',
+            'freeway',
+            'rural_highway',
+            'gravel',
+
+            // Resources
+            'car_id',
+            'supervisor_id'
+        );
+
+        Auth::user()->trips()->create($trip);
+
+        return ApiResponder::respondWithMessage('trip created')
+                             ->setStatusCode(201);
+    }
+}
