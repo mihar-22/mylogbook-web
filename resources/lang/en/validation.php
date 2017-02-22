556ad2eed7aad16995c44cb179afe80a2d3f6cb6
mihar-22 <rahim_alwer@hotmail.com>
2017-02-22 11:59:58 +1100

    add missing "." in alpha_num_space rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -89,7 +89,7 @@ return [
     // Extensions
 
     'alpha_space'          => 'The :attribute may only contain letters and spaces.',
-    'alpha_num_space'      => 'The :attribute may only contain letters, numbers and spaces',
+    'alpha_num_space'      => 'The :attribute may only contain letters, numbers and spaces.',
 
     /*
     |--------------------------------------------------------------------------
