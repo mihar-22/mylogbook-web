206e415d4391794ac004c67c531cfd0176bb5db7
mihar-22 <rahim_alwer@hotmail.com>
2017-04-21 18:13:38 +1000

    update home page, 404, 503 views

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-<div class="mdl-grid" style="padding-bottom: 64px;">		
+<div class="mdl-grid">		
 	<div class="mdl-cell mdl-cell--4-col-phone mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--6-col-desktop mdl-cell--3-offset-desktop"
 		 style="text-align: center;">
 
     	<img src="{{ asset('svg/503.svg') }}"
     		 alt="Page Not Found"
-    		 style="max-width: 540px;">
+    		 style="max-width: 540px; margin-top: -16px;">
 
     	<h1 class="mdl-typography--display-1">Service Unavailable</h1>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
-<div class="mdl-grid" style="padding-bottom: 64px;">		
+<div class="mdl-grid">		
 	<div class="mdl-cell mdl-cell--4-col-phone mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--6-col-desktop mdl-cell--3-offset-desktop"
 		 style="text-align: center;">
 
     	<img src="{{ asset('svg/coming-soon.svg') }}"
     		 alt="Page Not Found"
-    		 style="max-width: 540px;">
+    		 style="max-width: 500px;">
 
-    	<h1 class="mdl-typography--display-2" style="margin-top: 0;">Coming Soon</h1>
+    	<h1 class="mdl-typography--display-2" style="margin-top: 40px;">Coming Soon</h1>
 
     	<p class="mlb-typography--subheading">I know you're excited, just sit tight and wait!</p>
 	</div>
