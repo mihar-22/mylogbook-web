45e708d94cd8250ccb4a881965e1fa801c80c0b5
mihar-22 <rahim_alwer@hotmail.com>
2017-05-02 21:33:19 +1000

    build email verified success view

--- resources/views/auth/email/success.blade.php
+++ resources/views/auth/email/success.blade.php
@@ -1,14 +1,9 @@
 @extends('master')
 
 @section('content')
-<div class="mdl-grid">		
-	<div class="mdl-cell mdl-cell--4-col-phone mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--6-col-desktop mdl-cell--3-offset-desktop"
-		 style="text-align: center;">
 
-    	<img src="{{ asset('svg/email-verified-success.svg') }}"
-    		 alt="Email Verified"
-    		 style="max-width: 500px;">
-
-	</div>
+<div class="u-centerPiece">
+  <img src="{{ asset('svg/email-verified-success.svg') }}" alt="Page Not Found">
 </div>
+
 @endsection
