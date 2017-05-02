9ededd6b0462a084d8250bdd47b5180a809c0dd8
mihar-22 <rahim_alwer@hotmail.com>
2017-05-02 21:31:19 +1000

    build 503 view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,17 +1,13 @@
 @extends('master')
 
 @section('content')
-<div class="mdl-grid">		
-	<div class="mdl-cell mdl-cell--4-col-phone mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--6-col-desktop mdl-cell--3-offset-desktop"
-		 style="text-align: center;">
 
-    	<img src="{{ asset('svg/503.svg') }}"
-    		 alt="Page Not Found"
-    		 style="max-width: 540px; margin-top: -16px;">
+<div class="u-centerPiece">
+  <img src="{{ asset('svg/503.svg') }}" alt="Page Not Found">
 
-    	<h1 class="mdl-typography--display-1">Service Unavailable</h1>
+  <h1 class="md-display-1">Service Unavailable</h1>
 
-    	<p class="mlb-typography--subheading">We've encountered a problem, we are working hard to fix it!</p>
-	</div>
+  <p class="md-headline">We've encountered a problem, we are working hard to fix it!</p>
 </div>
+
 @endsection
