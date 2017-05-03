7e0a72fff4722d554e28b01a34f0445bec8607ea
mihar-22 <rahim_alwer@hotmail.com>
2017-05-03 18:16:50 +1000

    build coming soon view

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,1 +1,15 @@
 @extends('master')
+
+@section('content')
+
+<div class="u-centerPiece">
+  <img src="{{ asset('svg/coming-soon.svg') }}"
+       alt="Coming Soon"
+       style="max-width: 450px;">
+
+  <h1 class="md-display-1">Coming Soon</h1>
+
+  <p class="md-headline">Changing the way learner drivers log their hours.</p>
+</div>
+
+@endsection
